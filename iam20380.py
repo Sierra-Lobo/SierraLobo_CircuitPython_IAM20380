f27fe5f91b2de71740af6477c9f7cf4b248a82dd
caden_hillis <cadenjhillis@gmail.com>
2023-03-13 18:50:24 -0400

    Update iam20380.py

--- iam20380.py
+++ iam20380.py
@@ -12,191 +12,171 @@ from micropython import const
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_register.i2c_bits import ROBits, RWBits
 from adafruit_register.i2c_bit import ROBit, RWBit
-from adafruit_register.i2c_struct import UnaryStruct
+from adafruit_register.i2c_struct import UnaryStruct, ROUnaryStruct
+from typing import Tuple
+import time
 
-# Register Map
-#SELF_TEST_X_GYRO    = const(0x00)
-#SELF_TEST_Y_GYRO    = const(0x01)
-#SELF_TEST_Z_GYRO    = const(0x02)
-#XG_OFFS_USRH        = const(0x13)
-#XG_OFFS_USRL        = const(0x14)
-#YG_OFFS_USRH        = const(0x15)
-#YG_OFFS_USRL        = const(0x16)
-#ZG_OFFS_USRH        = const(0x17)
-#ZG_OFFS_USRL        = const(0x18)
-SMPLRT_DIV          = const(0x19)
-CONFIG              = const(0x1A)
-GYRO_CONFIG         = const(0x1B)
-LP_MODE_CFG         = const(0x1E)
-FIFO_EN             = const(0x23)
-#FSYNC_INT           = const(0x36)
-#INT_PIN_CFG         = const(0x37)
-INT_ENABLE          = const(0x38)
-INT_STATUS          = const(0x3A)
-TEMP_OUT_H          = const(0x41)
-TEMP_OUT_L          = const(0x42)
-GYRO_XOUT_H         = const(0x43)
-GYRO_XOUT_L         = const(0x44)
-GYRO_YOUT_H         = const(0x45)
-GYRO_YOUT_L         = const(0x46)
-GYRO_ZOUT_H         = const(0x47)
-GYRO_ZOUT_L         = const(0x48)
-#SIGNAL_PATH_RESET   = const(0x68)
-USER_CTRL           = const(0x6A)
-PWR_MGMT_1          = const(0x6B)
-PWR_MGMT_2          = const(0x6C)
-#FIFO_COUNTH         = const(0x72)
-#FIFO_COUNTL         = const(0x73)
-#FIFO_R_W            = const(0x74)
-WHO_AM_I            = const(0x75)
+_IAM20380_DEFAULT_ADDRESS = const(0x00)
+_IAM20380_WHO_AM_I = const(0xB5)
 
-# Default Register Values
-##PWR_MGMT_1_DEFAULT  = const(0x40)
-WHO_AM_I_DEFAULT    = const(0xB5) 
+_IAM20380_SMPLRT_DIV_REG = const(0x19)
+_IAM20380_CONFIG_REG = const(0x1A)
+_IAM20380_GYRO_CONFIG_REG = const(0x1B)
+_IAM20380_LP_MODE_CFG_REG = const(0x1E)
+_IAM20380_TEMP_OUT_H_REG = const(0x41)
+_IAM20380_GYRO_XOUT_H_REG = const(0x43)
+_IAM20380_PWR_MGMT_1_REG = const(0x6B)
+_IAM20380_WHO_AM_I_REG = const(0x75)
+
+_IAM20380_sens = [
+    """The sensitivity of the sensor per configured value, LSB/dps
+    determined by FS_SEL value
+    """
+    131,
+    65.5,
+    32.8,
+    16.4
+]
+
+_IAM20380_fs = [
+    """The offset of the sensor per configured value, dps
+    determined by FS_SEL value
+    """
+    250,
+    500,
+    1000,
+    2000
+]
+
+_IAM20380_avgs = [
+    """The number of averages of the sensor per configured value
+    determined by G_AVGCFG value
+    """
+    1,
+    2,
+    4,
+    8,
+    16,
+    32,
+    64,
+    128
+]
 
 class IAM20380:
 
-    # Class Variables (NOT ALL REGISTERS AND FUNCTIONS ARE IMPLEMENTED)
-    _smplrt_div = UnaryStruct(SMPLRT_DIV, "<B")
-    _dlpf_cfg = RWBits(3, CONFIG, 0)
-    _fs_sel = RWBits(2, GYRO_CONFIG, 3)
-    _fchoice_b = RWBits(2, GYRO_CONFIG, 0)
-    _gyro_cycle = RWBit(LP_MODE_CFG, 7)
-    _gavg_cfg = RWBits(3, LP_MODE_CFG, 4)
-    _drdy_int_en = RWBit(INT_ENABLE, 0)
-    _drdy_int = RWBit(INT_STATUS, 0)
-    _temp_h = UnaryStruct(TEMP_OUT_H, "<B")
-    _temp_l = UnaryStruct(TEMP_OUT_L, "<B")
-    _gyro_xout_h = UnaryStruct(GYRO_XOUT_H, "<B")
-    _gyro_xout_l = UnaryStruct(GYRO_XOUT_L, "<B")
-    _gyro_yout_h = UnaryStruct(GYRO_YOUT_H, "<B")
-    _gyro_yout_l = UnaryStruct(GYRO_YOUT_L, "<B")
-    _gyro_zout_h = UnaryStruct(GYRO_ZOUT_H, "<B")
-    _gyro_zout_l = UnaryStruct(GYRO_ZOUT_H, "<B")
-    _sig_cond_rst = RWBit(USER_CTRL, 0)
-    _device_reset = RWBit(PWR_MGMT_1, 7)
-    _sleep = RWBit(PWR_MGMT_1, 6)
-    _clksel = RWBits(3, PWR_MGMT_1, 0)
-    _who_am_i = ROBits(4, WHO_AM_I, 0)
-
-    class _BAD_WHO_AM_I(Exception):
-        pass
+    _chip_id = ROUnaryStruct(_IAM20380_WHO_AM_I_REG, "<B")
+    _reset = RWBit(_IAM20380_PWR_MGMT_1_REG, 7)
+    _fs_sel = RWBits(2, _IAM20380_GYRO_CONFIG_REG, 3)
+    _pwr_mgmt_1 = UnaryStruct(_IAM20380_PWR_MGMT_1_REG, "<B")
+    _gavg_cfg = RWBits(3, _IAM20380_LP_MODE_CFG_REG, 4)
+    _fchoice_b = RWBits(2, _IAM20380_GYRO_CONFIG_REG, 0)
+    _dlpf_cfg = RWBits(3, _IAM20380_CONFIG_REG, 0)
+    _smplrt_div = UnaryStruct(_IAM20380_SMPLRT_DIV_REG, "<B")
     
-    def __init__(self, i2c_bus, addr):
+    def __init__(self, i2c_bus, addr : int = _IAM20380_DEFAULT_ADDRESS) -> None:
         self.i2c_device = I2CDevice(i2c_bus, addr, probe=False)
-        test = self._who_am_i
-        if not test == 5: raise self._BAD_WHO_AM_I("[ERROR][IAM20380][BAD WHO_AM_I VALUE]: {}".format(test))
-        self.ON()
+        if self._chip_id != _IAM20380_WHO_AM_I:
+            raise RuntimeError("Failed to find IAM-20380 - check your wiring!")
+        
+        self._buffer = bytearray(6)
+        self.reset()
 
-    def ON(self):
-        #self.rst()
-        self._smplrt_div = 99 # 100Hz sampling, sampling rate = 1kHz/(1+smplrt_div)
-        self._dlpf_cfg = 4 #set to 20Hz LPF 1kHz sampling rate
-        self._fs_sel = 0 #+/- 250dps
-        self._fchoice_b = 0 #dlpf NOT bypassed
-        self._drdy_int_en = 1
-        self._sleep = 0 #it is intiallized as 1
-
-    def SLEEP(self):
-        self._sleep = 1
-
-    def rst(self):
-        self._device_reset = 1
-        i = self._device_reset
-        while i == 1:
-            i = self._device_reset #waits for reset to be completed
-
-    #returns python float (doubler precision flt)
-    @property
-    def read(self):
-        out = self.read_raw
-        for meas in range(len(out)):
-            if (out[meas]>>15==1):
-                out[meas] = ((out[meas] ^ 0xFFFF) + 1) *(-1)
-            out[meas] = float(out[meas]/131) # 131LSB/dps , hardcoded in sensitivty, sorry future self
-        return(out)
+    def reset(self) -> None:
+        """Reset the sensor to the default state set by the library"""
+        self._reset = True
+        time.sleep(0.020) #TODO: get the correct time for startup
+        #default settings of 1khz sampling rate, 128x averaging, ODR
+        self._pwr_mgmt_1 = 0x01
+        self._fchoice_b = 0x00
+        self.fs = 250
+        self.dlpf = 6
+        self.smplrt_div = 255
+        self.avgs = 128
 
     @property
-    def read_raw(self):
-        x = (self._gyro_xout_h << 8) + self._gyro_xout_l
-        y = (self._gyro_yout_h << 8) + self._gyro_yout_l
-        z = (self._gyro_zout_h << 8) + self._gyro_zout_l
-        out = [ x, y, z]
-        return(out)
+    def temperature(self) -> float:
+        """The processed temperature sensor value, returned in floating point C"""
+        self._buffer[0] = _IAM20380_TEMP_OUT_H_REG
+        with self.i2c_device as i2c:
+            i2c.write_then_readinto(self._buffer, self._buffer, out_end=1)
+        temp = self._buffer[0] << 8 | self._buffer[1]
+        temp /= 326.8
+        temp += 25 # not sure this is correct, datasheet p36 appears wrong
+        return temp
+    
+    @property
+    def rotation(self) -> Tuple[float, float, float]:
+        """The processed gyroscope sensor values.
+        A 3-tuple of X, Y, Z axis values in dps that are signed floats.
+        """
+        self._buffer[0] = _IAM20380_GYRO_XOUT_H_REG
+        with self.i2c_device as i2c:
+            i2c.write_then_readinto(self._buffer, self._buffer, out_end=1)
+        x = self._buffer[0] << 8 | self._buffer[1]
+        y = self._buffer[2] << 8 | self._buffer[3]
+        z = self._buffer[4] << 8 | self._buffer[5]
+        #fix center offset
+        x -= 1 << 15
+        y -= 1 << 15
+        z -= 1 << 15
+        #scale to dps
+        x /= self.sens
+        y /= self.sens
+        z /= self.sens
+
+    @property
+    def fs(self) -> int:
+        """The gyroscope full scale output, returned as dps"""
+        temp = self._fs_sel
+        return _IAM20380_fs[temp]
+    
+    @fs.setter
+    def fs(self, value: int) -> None:
+        if value not in _IAM20380_fs:
+            raise ValueError("Full scale must be 250, 500, 1000, or 2000 dps")
+        self._fs_sel = _IAM20380_fs.index(value)
+
+    @property
+    def sens(self) -> float:
+        """The gyroscope sensitivity, based upon FS_SEL, LSB/dps"""
+        temp = self._fs_sel
+        return _IAM20380_sens[temp]
         
     @property
-    def temp(self): # @25degc it reads 0
-        temp = (self._temp_h << 8) + (self._temp_l) # merge registers
-        #this is in twos comp when the datasheets says it isnt LOL
-        if temp>>15 == 1:
-            temp = ((temp ^ 0xFFFF) + 1) * (-1)
-        temp = (temp/326.8)+25 #this looks wrong??
-        return(temp)
+    def dlpf(self) -> int:
+        """The gyroscope dual low pass configure bits"""
+        return self._dlpf_cfg
 
-    # @property
-    # def smplrt_div(self):
-    #     #print("[IAM20380][SMPLRT_DIV][",hex(self._smplrt_div_data),"]")
-    #     return(self._smplrt_div)
-    # @smplrt_div.setter
-    # def smplrt_div(self, value):
-    #     self._smplrt_div = value
+    @dlpf.setter
+    def dlpf(self, value : int) -> None:
+        if value <= 1 | 6 <= value:
+            raise ValueError("DLPF must be acceptable value")
+        else:
+            self._dlpf_cfg = value
+        
+    @property
+    def smplrt_div(self) -> int:
+        """The gyroscope sample rate divider"""
+        return self._smplrt_div
+    
+    @smplrt_div.setter
+    def smplrt_div(self, value : int) -> None:
+        if 0<= value <= 255:
+            raise ValueError("SMPLRT_DIV must be 0-255")
 
-    # @property
-    # def dlpf_cfg(self):
-    #     #print("[IAM20380][CONFIG][DPLF_CFG][",bin(self._dplf_cfg),"]")
-    #     return(self._dlpf_cfg)
-    # @dlpf_cfg.setter
-    # def dlpf_cfg(self, value):
-    #     self._dlpf_cfg = value
+    @property
+    def avgs(self) -> int:
+        "Number of averages the gyro takes per measurement, values 0-7, averages 2^x"
+        temp = self._gavg_cfg
+        return _IAM20380_avgs[temp]
 
-    # @property
-    # def fs_sel(self):
-    #     #print("[IAM20380][GYRO_CONFIG][FS_SEL][",bin(self._fs_sel),"]")
-    #     return(self._fs_sel)
-    # @fs_sel.setter
-    # def fs_sel(self, value):
-    #     self._fs_sel = value
+    @avgs.setter
+    def avgs(self, value : int) -> None:
+        if value not in _IAM20380_avgs:
+            raise ValueError("Averages must be 1, 2, 4, 8, 16, 32, 64, or 128")
+        self._gavg_cfg = _IAM20380_avgs.index(value)
 
-    # @property
-    # def fchoice_b(self):
-    #     #print("[IAM20380][GYRO_CONFIG][FCHOICE_B][",bin(self._fchoice_b),"]")
-    #     return(self._fchoice_b)
-    # @fchoice_b.setter
-    # def fchoice_b(self, value):
-    #     self._fchoice_b = value
-
-    # @property
-    # def gyro_cycle(self):
-    #     #print("[IAM20380][LP_MODE_CFG][GYRO_CYCLE][",bin(self._gyro_cycle),"]")
-    #     return(self._gyro_cycle)
-    # @gyro_cycle.setter
-    # def gyro_cycle(self, value):
-    #     self._gyro_cycle = value
-
-    # @property
-    # def gavg_cfg(self):
-    #     #print("[IAM20380][LP_MODE_CFG][GAVG_CFG][",bin(self._gavg_cfg),"]")
-    #     return(self._gavg_cfg)
-    # @gavg_cfg.setter
-    # def gavg_cfg(self, value):
-    #     self._gavg_cfg = value
-
-    # def _gyro_scale(self, raw_input):
-    #     #scale is in form LSB/dps # I AM NOT SURE IF THIS CONVERSION IS CORRECCT
-    #     sw = self.fs_sel
-    #     if (sw == 0): # +/- 250 dps
-    #         sensitivity=131
-    #         scale = 250
-    #     elif (sw == 1): # +/- 500 dps
-    #         sensitivity=65.5
-    #         scale = 500
-    #     elif (sw == 2): # +/- 1000 dps
-    #         sensitivity=32.8
-    #         scale = 1000
-    #     else: # +/- 2000 dps
-    #         sensitivity=16.4
-    #         scale = 2000
-    #     self._16btwos2int(raw_input)
-    #     scaled_output = raw_input / scale #/ sensitivity # THIS IS W R O N G FIX LATER
-    #     #print(scaled_output)
-    #     return(scaled_output)
+    @property
+    def odr(self) -> int:
+        temp =  (1000 / self._smplrt_div)
+        return temp
